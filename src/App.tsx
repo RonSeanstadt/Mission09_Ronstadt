ce3f606ae891c09df6b4ceded9f6bf55dde433cc
RonSeanstadt <sronstadt470@gmail.com>
2024-03-08 15:14:56 -0700

    Finished all of Mission 9!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import teams from './CollegeBasketballTeams.json';
 import './App.css';
+
+type TeamData = {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+};
+
+// Team card creater class
+class Team extends React.Component<TeamData> {
+  render() {
+    const { school, name, city, state } = this.props;
+    return (
+      <div className="Team-Style">
+        <h2>
+          <b>School:</b> {school}
+        </h2>
+        <h3>
+          <b>Mascot:</b> {name}
+        </h3>
+        <h3>
+          <b>Location:</b> {city}, {state}
+        </h3>
+        <br />
+      </div>
+    );
+  }
+}
+
+// Team card list function
+function TeamList() {
+  const [teamData, setTeamData] = useState<TeamData[]>([]);
+
+  useEffect(() => {
+    // Set the data from the imported JSON directly
+    setTeamData(teams.teams);
+  }, []);
+
+  return (
+    <div className="TeamList">
+      {teamData.map((team, index) => (
+        <Team
+          key={index}
+          school={team.school}
+          name={team.name}
+          city={team.city}
+          state={team.state}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Welcome() {
+  return (
+    <h1>
+      This page lists out all of the NCAA basketball teams and their information
+    </h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
